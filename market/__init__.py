9cf28602fdf9e90dc36baa50be42efbdd24ba3ad
Sergey Stetsiuk <serg2033@gmail.com>
2022-02-11 20:42:21 +0300

    +forms, +validations, more dev

--- market/__init__.py
+++ market/__init__.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///market.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['SECRET_KEY'] = 'd3190be6959002fee8f296a1'
 db = SQLAlchemy(app)
 
 from market import routes
