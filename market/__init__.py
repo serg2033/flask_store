f36e548bfa26832ccae2f5713eda6d1fee85eaa1
Sergey Stetsiuk <serg2033@gmail.com>
2022-02-12 17:29:29 +0300

    user authentication

--- market/__init__.py
+++ market/__init__.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///market.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config['SECRET_KEY'] = 'd3190be6959002fee8f296a1'
+app.config["SECRET_KEY"] = "d3190be6959002fee8f296a1"
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from market import routes
--- market/routes.py
+++ market/routes.py
@@ -1,8 +1,9 @@
 from market import app
-from flask import render_template, redirect, url_for, flash, get_flashed_messages
+from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
-from market.forms import RegisterForm
+from market.forms import RegisterForm, LoginForm
 from market import db
+from flask_login import login_user
 
 
 @app.route("/")
@@ -17,19 +18,38 @@ def market():
     return render_template("market.html", items=items)
 
 
-@app.route("/register", methods=['GET', 'POST'])
+@app.route("/register", methods=["GET", "POST"])
 def register():
     form = RegisterForm()
     if form.validate_on_submit():
         user_to_create = User(
             username=form.username.data,
             email_address=form.email_address.data,
-            password_hash=form.password1.data,
+            password=form.password1.data,
         )
         db.session.add(user_to_create)
         db.session.commit()
-        return redirect(url_for('market'))
-    if form.errors != {}: # если нет ошибок из валидации формы
+        return redirect(url_for("market"))
+    if form.errors != {}:  # если нет ошибок из валидации формы
         for err_msg in form.errors.values():
-            flash(f'-----Error------: {err_msg}', category='danger')
+            flash(f"***Error***: {err_msg}", category="danger")
     return render_template("register.html", form=form)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        attempted_user = User.query.filter_by(username=form.username.data).first()
+        if attempted_user and attempted_user.check_password_correction(
+            attempted_password=form.password.data
+        ):
+            login_user(attempted_user)
+            flash(
+                f"Success. You are logged in as: {attempted_user.username}",
+                category="success",
+            )
+            return redirect(url_for("market"))
+        else:
+            flash("Username and password are not match.", category="danger")
+    return render_template("login.html", form=form)
